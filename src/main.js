253aaee06154297bd75d76da319dcec2c20a50b4
Borys <undying4@gmail.com>
2020-06-28 14:51:02 +0200

    Setup connection with DOM elements and add event listeners

---
+++ src/main.js
@@ -0,0 +1,39 @@
+const viewElems = {};
+
+const getDOMElem = id => {
+    return document.getElementById(id);
+}
+
+const connectHTMLElems = () => {
+    viewElems.mainContainer = getDOMElem('mainContainer');
+    viewElems.weatherSearchView = getDOMElem('weatherSearchView');
+    viewElems.weatherForecastView = getDOMElem('weatherForecastView');
+
+    viewElems.searchInput = getDOMElem('searchInput');
+    viewElems.searchButton = getDOMElem('searchButton');
+    viewElems.weatherCityContainer = getDOMElem('weatherCityContainer');
+
+    viewElems.weatherCity = getDOMElem('weatherCity');
+    viewElems.weatherIcon = getDOMElem('weatherIcon');
+
+    viewElems.weatherCurrentTemp = getDOMElem('weatherCurrentTemp');
+    viewElems.weatherMaxTemp = getDOMElem('weatherMaxTemp');
+    viewElems.weatherMinTemp = getDOMElem('weatherMinTemp');
+
+    viewElems.returnToSearchBtn = getDOMElem('returnToSearchBtn');
+}
+
+const setupListeners = () => {
+    viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
+    viewElems.searchButton.addEventListener('click', onSubmit);
+}
+
+const initializeApp = () => {
+    connectHTMLElems();
+    setupListeners();
+}
+
+const onSubmit = () => { }
+const onEnterSubmit = () => { }
+
+document.addEventListener('DOMContentLoaded', initializeApp);
